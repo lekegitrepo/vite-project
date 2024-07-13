dd3862d2e1f00ffe7937a3f79c58ddacca4f45dc
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-07-13 20:56:54 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import Counter from "./Counter.jsx";
+import MessageLength from "./MessageLength.jsx";
+import "./index.css";
+import ChangeColor from "./ChangeColor.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App firstname="Reece" lastname="James" /> */}
+    {/* <Counter /> */}
+    <MessageLength message="This is a long message" />
+    <ChangeColor />
+  </React.StrictMode>
+);
