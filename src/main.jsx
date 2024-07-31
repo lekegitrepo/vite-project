4024b99f4403b2c655ba9d8a5ac12f2c0b041533
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-07-31 18:34:13 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
 // import Counter from "./Counter.jsx";
-import MessageLength from "./MessageLength.jsx";
+// import MessageLength from "./MessageLength.jsx";
 import "./index.css";
-import ChangeColor from "./ChangeColor.jsx";
+// import ChangeColor from "./ChangeColor.jsx";
+// import UserRegistration from "./UserRegistration.jsx";
+// import UserInput from "./UserInput.jsx";
+// import TodoApp from "./TodoApp.jsx";
+// import Nasa from "./Nasa.jsx";
+// import UserList from "./UserList";
+// import Trivia from "./Trivia";
+// import User from "./User";
+import GithubApi from "./GithubApi";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App firstname="Reece" lastname="James" /> */}
     {/* <Counter /> */}
-    <MessageLength message="This is a long message" />
-    <ChangeColor />
+    {/* <MessageLength message="This is a long message" /> */}
+    {/* <ChangeColor /> */}
+    {/* <UserRegistration />
+    <UserInput />
+    <TodoApp /> */}
+    {/* <Nasa /> */}
+    {/* <UserList /> */}
+    {/* <Trivia /> */}
+    {/* <User /> */}
+    <GithubApi />
   </React.StrictMode>
 );
