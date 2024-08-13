a44a52630b1978b27a0d49ec34f86bd000fe3585
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-08-13 11:07:55 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import Counter from "./Counter.jsx";
-// import MessageLength from "./MessageLength.jsx";
-import "./index.css";
-// import ChangeColor from "./ChangeColor.jsx";
-// import UserRegistration from "./UserRegistration.jsx";
-// import UserInput from "./UserInput.jsx";
-// import TodoApp from "./TodoApp.jsx";
-// import Nasa from "./Nasa.jsx";
-// import UserList from "./UserList";
-// import Trivia from "./Trivia";
-// import User from "./User";
-import GithubApi from "./GithubApi";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Bookstore } from './pages/Bookstore'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App firstname="Reece" lastname="James" /> */}
-    {/* <Counter /> */}
-    {/* <MessageLength message="This is a long message" /> */}
-    {/* <ChangeColor /> */}
-    {/* <UserRegistration />
-    <UserInput />
-    <TodoApp /> */}
-    {/* <Nasa /> */}
-    {/* <UserList /> */}
-    {/* <Trivia /> */}
-    {/* <User /> */}
-    <GithubApi />
+    <Bookstore />
   </React.StrictMode>
-);
+)
